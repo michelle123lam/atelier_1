7f4b6b265962d8db70470b10449bbb8ab59a5cad
Michelle Lam <michelle123lam@gmail.com>
2015-07-02 09:36:02 -0700

    bundle updating sqlite3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap.js
+//= require semantic-ui/dist/semantic.js
+//= require atelier.js
---
+++ app/assets/javascripts/atelier.js
@@ -0,0 +1,3 @@
+$('#example2').progress({
+  percent: 60
+});
